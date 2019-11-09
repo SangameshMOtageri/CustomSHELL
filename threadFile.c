0d8af5fcd573f9a4014cb66d7e72e8e3f5bac4dd
SangameshMOtageri <45513969+SangameshMOtageri@users.noreply.github.com>
2019-11-09 15:28:29 +0530

    Update threadFile.c

--- threadFile.c
+++ threadFile.c
@@ -1,8 +1,15 @@
+  
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include<string.h>
+#include<time.h>
 
 void ls_func(){
+    clock_t c = clock();
+    printf("CP");
+    while(clock() < c + 100000)
+	printf("..");
     printf("\nls was called");
 }
 void mkdir_func(){
@@ -11,18 +18,25 @@ void mkdir_func(){
 int main(){
     char command[20];
     char *api_extension = "_func";
+    char success_msg[10] =  "ChildDone";
+    char read_success_msg[10];
     void *avail_command_ptr[2] = {&ls_func, &mkdir_func};
     char *avail_command_name[2] = {"ls_func", "mkdir_func"};
     unsigned int command_count = 2;
+    int pipeID[2];
+
     printf("Welcome to CMD");
     while(1){
-        printf("\nEnter command>>$$>>:");
+	pipe(pipeID);
+        printf("\n>>$$>>:");
         gets(command);
         if(strcmp(command, "exit") == 0)
             break;
         //Master process to maintain the CMD prompt
         //Child process to run the command entered by the user.
         if(fork() == 0){
+	    //Close the read end of pipe
+	    close(pipeID[0]);
             strcat(command, api_extension);
             for(int i = 0; i < command_count; i++){
                 if(strcmp(command, avail_command_name[i]) == 0){
@@ -31,7 +45,20 @@ int main(){
                     break;
                 }
             }
+	    write(pipeID[1], success_msg, sizeof(success_msg));
+	    close(pipeID[1]);
+        break;
         }
-    }
-    getchar();
+        else{
+	    printf("PP@");
+	    //Close the write end of pipe
+	    close(pipeID[1]);
+	    while(strcmp(read_success_msg, "ChildDone")){
+		printf("\n PP");
+		read(pipeID[0], read_success_msg, sizeof(read_success_msg));
+	    }
+	    close(pipeID[0]);
+	    wait(NULL);
+        }
+     }
 }
